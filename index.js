921b100981ef6f89bf87598350a8b449f1320764
ben awad <benawad97@gmail.com>
2017-10-11 23:11:32 -0500

    add cors *

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import cors from 'cors';
 
 import models from './models';
 
@@ -17,6 +18,8 @@ const schema = makeExecutableSchema({
 });
 
 const app = express();
+
+app.use(cors('*'));
 
 const graphqlEndpoint = '/graphql';
 
