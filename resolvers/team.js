ca5876e8bc592c8102a8f35ceaa6f8b2336898b2
ben awad <benawad97@gmail.com>
2017-11-20 00:59:03 -0600

    fix transactions

--- resolvers/team.js
+++ resolvers/team.js
@@ -48,10 +48,16 @@ export default {
     }),
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        const response = await models.sequelize.transaction(async () => {
-          const team = await models.Team.create({ ...args });
-          await models.Channel.create({ name: 'general', public: true, teamId: team.id });
-          await models.Member.create({ teamId: team.id, userId: user.id, admin: true });
+        const response = await models.sequelize.transaction(async (transaction) => {
+          const team = await models.Team.create({ ...args }, { transaction });
+          await models.Channel.create(
+            { name: 'general', public: true, teamId: team.id },
+            { transaction },
+          );
+          await models.Member.create(
+            { teamId: team.id, userId: user.id, admin: true },
+            { transaction },
+          );
           return team;
         });
         return {
