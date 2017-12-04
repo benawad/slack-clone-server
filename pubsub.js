7071cb9d462ae9676f0bf58665367a7fbcd784fc
ben awad <benawad97@gmail.com>
2017-12-04 11:03:05 -0600

    use redis pubsub

--- pubsub.js
+++ pubsub.js
@@ -1,3 +1,9 @@
-import { PubSub } from 'graphql-subscriptions';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 
-export default new PubSub();
+export default new RedisPubSub({
+  connection: {
+    host: '127.0.0.1',
+    port: 6379,
+    retry_strategy: options => Math.max(options.attempt * 100, 3000),
+  },
+});
